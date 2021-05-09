5364dd333506cdfa4d06bd960c884f7582039463
David Letaw <jdletaw@gmail.com>
2021-05-08 18:25:54 -0700

    creates daily menu

---
+++ src/Components/AddDailyMenu.js
@@ -0,0 +1,33 @@
+import {MdDeleteForever} from "react-icons/md";
+import { useState} from "react";
+
+const AddDailyMenu = ({ handleAddDailyMenu }) => {
+    const [menuItem, setMenuItem] = useState('')
+
+    const handleChange = (event) => {
+        setMenuItem(event.target.value);
+    };
+
+    const handleSaveClick = () => {
+        console.log('clicked');
+        handleAddDailyMenu(menuItem);
+    };
+
+    return (
+        <div className="daily-menu new">
+            <textarea
+                rows='8'
+                cols='10'
+                placeholder='Add new daily menu here...'
+                value={menuItem}
+                onChange={handleChange}
+            />
+            <div className='daily-menu-footer'>
+                <button className='save' onClick={handleSaveClick}>Save</button>
+                <MdDeleteForever className='delete-icon' size='1.3em' />
+            </div>
+        </div>
+    )
+}
+
+export default AddDailyMenu;
