172459657dcc18605ff3fe0a5ec38cce035c34ce
David Letaw <jdletaw@gmail.com>
2021-05-09 22:28:39 -0700

    Fixes search component state change

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -6,11 +6,11 @@ const Search = ({ handleSearchNote }) => {
         <div className='search'>
             <MdSearch className='search-icons' size='1.3em' />
             <input
-                onChange={(event)=>
+                onChange={(event) =>
                     handleSearchNote(event.target.value)
                 }
                 type='text'
-                placeholder='type to search...'/>
+                placeholder='search...'/>
         </div>
     );
 }
